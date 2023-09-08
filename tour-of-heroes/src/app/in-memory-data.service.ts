819a3f7c96cd7925882d23b206573a7e0f88d273
João Vitor Veronese Vieira <joao.veronese@timeprimo.com>
2023-09-08 09:35:59 -0300

    feat(tour-of-heroes): 6 - simulate a data server

---
+++ tour-of-heroes/src/app/in-memory-data.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService, RequestInfo } from 'angular-in-memory-web-api';
+import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  constructor() {}
+
+  createDb() {
+    return { HEROES };
+  }
+
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0
+      ? Math.max(...heroes.map((hero) => hero.id)) + 1
+      : 11;
+  }
+}
